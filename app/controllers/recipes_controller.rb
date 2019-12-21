673ab704b405c7d7104b95c51028b348b45473d4
Eddie Kao <eddie@5xruby.tw>
2019-12-21 15:49:06 +0800

    finish CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,15 @@
 class RecipesController < ApplicationController
 
   def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    begin
+      @recipe = Recipe.find(params[:id])
+    rescue
+      render file: 'public/404.html'
+    end
   end
 
   def new
@@ -16,6 +25,41 @@ class RecipesController < ApplicationController
       redirect_to "/"
     else
       render :new   # app/views/recipes/new.html.erb
+    end
+  end
+
+  def edit
+    begin
+      @recipe = Recipe.find(params[:id])
+    rescue
+      render file: 'public/404.html'
+    end
+  end
+
+  def update
+    clean_params = params.require(:recipe).permit(:title, :description)
+
+    begin
+      @recipe = Recipe.find(params[:id])
+
+      if @recipe.update(clean_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+
+    rescue
+      render file: 'public/404.html'
+    end
+  end
+
+  def destroy
+    begin
+      @recipe = Recipe.find(params[:id])
+      @recipe.destroy
+      redirect_to root_path
+    rescue
+      render file: 'public/404.html'
     end
   end
 end
