a7def5a8e20a3d5bb10905e6ceeb55cbf00092a4
Eddie Kao <eddie@5xruby.tw>
2020-01-04 14:36:06 +0800

    Cart part 5

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart
-  def initialize
-    @items = []
+  def initialize(items = [])
+    @items = items
   end
 
   def add_item(product_id)
@@ -19,6 +19,29 @@ class Cart
 
   def items
     @items
+  end
+
+  def total_price
+    @items.reduce(0) { |sum, item| sum + item.total_price }
+  end
+
+  def serialize
+    { 
+      "items" => @items.map { |item| 
+        { "product_id" => item.product_id, "quantity" => item.quantity }
+      } 
+    }
+  end
+
+  def self.from_hash(hash = nil)
+    if hash && hash["items"]
+      items = hash["items"].map { |item|
+        CartItem.new(item["product_id"], item["quantity"])
+      }
+      Cart.new(items)
+    else
+      Cart.new
+    end
   end
 end
 
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -41,7 +41,52 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.first.total_price).to be 30
       expect(cart.items.last.total_price).to be 40
     end
+
+    it "可以計算整台購物車的總消費金額" do
+      cart = Cart.new
+      r1 = create(:recipe, price: 10)
+      r2 = create(:recipe, price: 20)
+
+      3.times { cart.add_item(r1.id) }
+      2.times { cart.add_item(r2.id) }
+
+      expect(cart.total_price).to be 70
+    end
+  end
+
+  context "進階功能" do
+    it "可以將購物車內容轉換成 Hash 並存到 Session 裡" do
+      cart = Cart.new
+      r1 = create(:recipe)
+      r2 = create(:recipe)
+
+      3.times { cart.add_item(r1.id) }
+      2.times { cart.add_item(r2.id) }
+
+      expect(cart.serialize).to eq cart_hash
+    end
+
+    it "存放在 Session 的內容（Hash 格式），還原成購物車的內容。" do
+      # Arrange / Act
+      cart = Cart.from_hash(cart_hash)
+
+      # Assert
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.product_id).to be 1
+    end
+  end
+
+  private
+
+  def cart_hash
+    {
+      "items" => [
+        { "product_id" => 1, "quantity" => 3}, 
+        { "product_id" => 2, "quantity" => 2}, 
+      ]
+    }
   end
 end
 
-# 可以計算整台購物車的總消費金額。
+# 
+# 
